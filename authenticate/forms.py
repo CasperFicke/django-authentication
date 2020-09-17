1f6468e9ed258fd9c31056e29367c02adecfea9d
CasperFicke <cficke@quicknet.nl>
2020-09-17 15:44:51 +0200

    add user registration

---
+++ authenticate/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+# Sign up form
+class SignUpForm(UserCreationForm):
+  email = forms.EmailField(
+    label= "hier uw mailadres",
+    widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'email'  })
+  )
+  first_name = forms.CharField(
+    max_length=100,
+    label='',
+    widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'first name'}),
+    help_text="<small>Enter your first name here</small>"
+  )
+  last_name  = forms.CharField(
+    max_length=100,
+    widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'lastname'})
+  )
+
+  class Meta:
+    model = User
+    fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+  
+  def __init__(self, *args, **kwargs):
+    super(SignUpForm, self).__init__(*args, **kwargs)
+
+    self.fields['username'].widget.attrs['class'] = 'form-control'
+    # self.fields['username'].label = 'hier uw gebruikersnaam'
+    self.fields['username'].label = ''
+    self.fields['username'].widget.attrs['placeholder'] = 'username'
+    self.fields['username'].help_text = '<small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small>'
+
+    self.fields['password1'].widget.attrs['class'] = 'form-control'
+    self.fields['password1'].label = ''
+    self.fields['password1'].widget.attrs['placeholder'] = 'password'
+
+    self.fields['password2'].widget.attrs['class'] = 'form-control'
+    self.fields['password2'].label = ''
+    self.fields['password2'].widget.attrs['placeholder'] = 'confirm password'
+    self.fields['password2'].help_text = '<small>Enter the same password as before, for verification.</small>'
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import SignUpForm
 
 # home view
 def home(request):
@@ -12,6 +15,25 @@ def about(request):
   mylast_name  = 'Ficke'
   context = {'first_name': myfirst_name, 'last_name': mylast_name}
   return render(request, 'authenticate/about.html', context)
+
+# register view
+def register_user(request):
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password1']
+      user = authenticate(request, username=username, password=password)
+      login(request, user)
+      messages.success(request, ("You're logged in"))
+      return redirect('home')
+  else:
+    form = SignUpForm()
+  
+  context = {'form': form}
+  return render(request, 'authenticate/register.html', context)
+
 
 # login view
 def login_user(request):
