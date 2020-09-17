95679b45a2b804d1db3de20d90362977e82bea10
CasperFicke <cficke@quicknet.nl>
2020-09-17 12:24:46 +0200

    add login and logout funtionality

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # home view
 def home(request):
@@ -10,3 +12,25 @@ def about(request):
   mylast_name  = 'Ficke'
   context = {'first_name': myfirst_name, 'last_name': mylast_name}
   return render(request, 'authenticate/about.html', context)
+
+# login view
+def login_user(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      messages.success(request, ("You're logged in"))
+      return redirect('home')
+    else:
+      messages.success(request, ("Invalid username or password!"))
+      return redirect('login')
+  else:
+    return render(request, 'authenticate/login.html', {})
+
+# logout view
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You're logged out"))
+    return redirect('home')
